7db2e95bc45bbfe190a15f589f81624c04eb6370
심보현 <bh.sim@oksetro.com>
2025-01-16 09:26:28 +0900

    20250116

---
+++ adapter-out/src/test/java/org/example/adapterout/AdapterOutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adapterout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
